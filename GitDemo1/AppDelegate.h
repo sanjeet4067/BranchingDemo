449280c87d74c945ca79579056ef656d6a822f7c
sanjeet4067 <sanjeet4067@gmail.com>
2017-07-06 13:06:50 +0530

    Initial Commit

---
+++ GitDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo1
+//
+//  Created by Coral  on 06/07/17.
+//  Copyright © 2017 Coral . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDemo1
+//
+//  Created by Coral  on 06/07/17.
+//  Copyright © 2017 Coral . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
